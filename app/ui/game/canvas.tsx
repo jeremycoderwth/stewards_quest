b489de77e8c6cf422964e77b444c60caf92235a2
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-09 00:22:02 +0800

    Component: fixed canvas rednering

--- app/ui/game/canvas.tsx
+++ app/ui/game/canvas.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import kaplay from 'kaplay';
+import kaplay, { KAPLAYCtx } from 'kaplay';
 import { loadAssets } from '@/app/lib/game/contexts'; 
 import { registerScenes } from '@/app/kaplay/game/scenes';
 import { useGameStore } from '@/app/lib/store/store-data';
 import type { Character } from '@/app/lib/definitions';
 
 export default function GameCanvas() {
+    const characterSelectRef = useRef<HTMLDivElement>(null);
+    const kaplayRef = useRef<KAPLAYCtx>(null);
+
     const router = useRouter();
     const setCharacter = useGameStore((s) => s.setCharacter);
 
     useEffect(() => {
+        const characterSelectElement = characterSelectRef.current;
+        if (!characterSelectElement) return;
+        if (kaplayRef.current) return;
+
         const k = kaplay({
             width: 380,
             height: 330,
@@ -20,7 +27,10 @@ export default function GameCanvas() {
             crisp: true,
             background: [0, 0, 0, 0], // #000000 for testing
             canvas: document.getElementById('kaplay-container') as HTMLCanvasElement,
+            root: characterSelectRef.current || undefined,
         });
+
+        kaplayRef.current = k;
 
         loadAssets(k);
         registerScenes(k, {
@@ -30,12 +40,22 @@ export default function GameCanvas() {
             },
         });
 
-        // show the character seleection
+        // show the character selection
         k.go('characterSelect');
+
+        return () => {
+            const canvas = characterSelectElement?.querySelector('canvas');
+
+            if (canvas) {
+                canvas.remove();
+            }
+
+            kaplayRef.current = null;
+        };
     }, [router, setCharacter]);
 
     return (
-        <canvas id="kaplay-container" />
+        <div ref={characterSelectRef} />
     );
 }
 
