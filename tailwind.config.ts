b5ef44986125355aff4b08680cd9bbfbdf337cd2
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-20 04:35:39 +0800

    Downloaded some stuff (i forgot) then  some config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                "slide-in": {
+                    "0%": { opacity: 0, transform: "translateX(100%)" },
+                    "100%": { opacity: 1, transform: "translateX(0)" },
+                },
+            },
+            animation: {
+                "slide-in": "slide-in 0.3s ease-out",
+            },
+        },
     },
     plugins: [],
 };
