2343904e6ac77b053a8c7c5c4451acec33f7f37b
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-20 04:32:25 +0800

    Deleted some uneccessary codes

--- app/kaplay/game/scenes.ts
+++ app/kaplay/game/scenes.ts
@@ -1,14 +1,7 @@
 import { characterSelectScene } from "@/app/kaplay/scenes/character-select";
-import { loadingScreen } from '@/app/kaplay/scenes/loading';
-import { instructionsScene } from '@/app/kaplay/scenes/instructions';
 import type { KAPLAYCtx } from "kaplay";
 import type { Character } from "@/app/lib/definitions";
 
 export function registerScenes(k: KAPLAYCtx, callbacks: { onCharacterConfirm: (character: Character) => void }) {
     k.scene("characterSelect", () => characterSelectScene(k, callbacks.onCharacterConfirm));
-}
-
-export function registerMechanicsScenes(k: KAPLAYCtx) {
-    loadingScreen(k);
-    instructionsScene(k);
 }
