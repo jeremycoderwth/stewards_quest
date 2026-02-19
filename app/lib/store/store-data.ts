b815e1c17497723dc4f4404eac2e0a76e358514b
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-20 04:34:42 +0800

    Added new store for data

--- app/lib/store/store-data.ts
+++ app/lib/store/store-data.ts
@@ -1,8 +1,18 @@
 import { create } from 'zustand';
 
-import type { GameState } from '@/app/lib/definitions';
+import type { GameState, Character } from '@/app/lib/definitions';
+
+type CharacterStore = {
+    selectedCharacter: Character | null;
+    setCharacter: (char: Character) => void;
+};
 
 export const useGameStore = create<GameState>((set) => ({
+    selectedCharacter: null,
+    setCharacter: (char) => set({ selectedCharacter: char }),
+}));
+
+export const useCharacterStore = create<CharacterStore>((set) => ({
     selectedCharacter: null,
     setCharacter: (char) => set({ selectedCharacter: char }),
 }));
