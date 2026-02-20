494eacd3508e80687ee691d7b87902ce4567b34e
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-21 01:05:34 +0800

    Fixed typo and checked code

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"strokeWidth="4"></circle>
                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
             </svg>
-            <span className="ml-3 text-lg">Loading profile...</span>
+            <span className="ml-3 text-lg">Loading game...</span>
         </div>
     );
 }
