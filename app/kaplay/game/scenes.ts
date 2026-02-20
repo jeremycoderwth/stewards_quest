ab1906d1eb9a3c967e4ab5f82058af125d6a1463
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-21 01:16:14 +0800

    Deleted unnecessary files and unused codes

--- app/kaplay/game/scenes.ts
+++
@@ -1,7 +0,0 @@
-import { characterSelectScene } from "@/app/kaplay/scenes/character-select";
-import type { KAPLAYCtx } from "kaplay";
-import type { Character } from "@/app/lib/definitions";
-
-export function registerScenes(k: KAPLAYCtx, callbacks: { onCharacterConfirm: (character: Character) => void }) {
-    k.scene("characterSelect", () => characterSelectScene(k, callbacks.onCharacterConfirm));
-}
