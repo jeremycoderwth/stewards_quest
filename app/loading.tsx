2bd85c9ea8aa4f45b41cdbb5510c47294c2492ab
jeremycoderwth <randeljeremy123@gmail.com>
2026-02-21 01:39:51 +0800

    Change stroke and fill color of loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,10 +3,10 @@ export default function Loading() {
     return (
         <div className="flex items-center justify-center h-screen">
             <svg className="animate-spin h-8 w-8 text-green-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"strokeWidth="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="white" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="white" d="M4 12a8 8 0 018-8v8H4z"></path>
             </svg>
-            <span className="ml-3 text-lg">Loading game...</span>
+            <span className="ml-3 text-lg text-white">Loading game...</span>
         </div>
     );
 }
